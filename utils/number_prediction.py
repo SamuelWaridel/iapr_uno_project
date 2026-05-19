6b03598d5df31d70f710ff74da14761869227753
SamuelWaridel <114981092+SamuelWaridel@users.noreply.github.com>
2026-05-19 15:12:22 +0200

    Simplified CNN

--- utils/number_prediction.py
+++ utils/number_prediction.py
@@ -112,64 +112,27 @@ class UnoCNN(nn.Module):
 
         self.features = nn.Sequential(
 
-            # Block 1
             nn.Conv2d(1, 32, 3, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
-
-            nn.Conv2d(32, 32, 3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-
             nn.MaxPool2d(2),
-            nn.Dropout2d(0.1),
 
-            # Block 2
             nn.Conv2d(32, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
             nn.ReLU(),
-
-            nn.Conv2d(64, 64, 3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-
             nn.MaxPool2d(2),
-            nn.Dropout2d(0.15),
 
-            # Block 3
             nn.Conv2d(64, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
             nn.ReLU(),
 
-            nn.Conv2d(128, 128, 3, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-
-            nn.MaxPool2d(2),
-            nn.Dropout2d(0.2),
-
-            # Block 4
-            nn.Conv2d(128, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-
-            nn.Conv2d(256, 256, 3, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-
-            nn.AdaptiveAvgPool2d((4, 4))
+            nn.AdaptiveAvgPool2d((4,4))
         )
 
         self.classifier = nn.Sequential(
 
             nn.Flatten(),
 
-            nn.Linear(256 * 4 * 4, 512),
+            nn.Linear(128*4*4, 128),
             nn.ReLU(),
-            nn.Dropout(0.5),
 
-            nn.Linear(512, 128),
-            nn.ReLU(),
             nn.Dropout(0.3),
 
             nn.Linear(128, n_classes)
